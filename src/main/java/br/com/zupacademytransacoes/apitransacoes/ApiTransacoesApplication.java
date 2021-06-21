d31a15e5f66546f555f79cfca98ef29cc584c6d3
William Bohn <williamxavero@gmail.com>
2021-06-21 09:36:44 -0300

    setup do projeto

---
+++ src/main/java/br/com/zupacademytransacoes/apitransacoes/ApiTransacoesApplication.java
@@ -0,0 +1,13 @@
+package br.com.zupacademytransacoes.apitransacoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTransacoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTransacoesApplication.class, args);
+	}
+
+}
